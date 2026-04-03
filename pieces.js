422a3013a6ac99e67532bcadcfa784621ea58813
maxime38955 <maxime38955@icloud.com>
2026-04-03 17:00:52 +0200

    Exo 4 Trie dans la consol avec le décroisant et le filte sur la desc

--- pieces.js
+++ pieces.js
@@ -5,6 +5,7 @@ const pieces = await reponse.json();
 const article = pieces[0];
 
 
+
 for (let i = 0; i < pieces.length; i++) {
  
 // Récupération de l'élément du DOM qui accueillera les fiches
@@ -35,12 +36,18 @@ pieceElement.appendChild(prixElement);
 pieceElement.appendChild(categorieElement);
 pieceElement.appendChild(descriptionElement);
 pieceElement.appendChild(disponibiliteElement);
+
+
+
+
+
+
  
 }
 
 
 // Gestion des boutons
-const boutonTrier = document.querySelector(".btn-trier");
+const boutonTrier = document.querySelector(".btn-trier-asc");
 
 boutonTrier.addEventListener("click", function () {
   const piecesOrdonnees = Array.from(pieces);
@@ -52,12 +59,38 @@ boutonTrier.addEventListener("click", function () {
   console.log(piecesOrdonnees);
 });
 
-const boutonFiltrer = document.querySelector(".btn-filtrer");
+// Gestion des boutons
+const boutonTrierDesc = document.querySelector(".btn-trier-desc");
 
+boutonTrierDesc.addEventListener("click", function () {
+  const piecesOrdonnees = Array.from(pieces);
+
+  piecesOrdonnees.sort(function (a, b) {
+    return b.prix - a.prix;
+  });
+
+  console.log(piecesOrdonnees);
+});
+
+
+
+const boutonFiltrer = document.querySelector(".btn-filtrer-non-abo");
 boutonFiltrer.addEventListener("click", function () {
    const piecesFiltrees = pieces.filter(function (piece) {
        return piece.prix <= 35;
    });
   console.log(piecesFiltrees);
+});
+
+const boutonFiltrerSan = document.querySelector(".btn-filtrer-sans-desc");
+
+boutonFiltrerSan.addEventListener("click", function () {
+   const piecesFiltrees = pieces.filter(function (piece) {
+       return piece.description;
+   });
+  console.log(piecesFiltrees);
 
 });
+
+
+
