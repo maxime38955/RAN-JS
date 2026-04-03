8e84d726c340f313ab4d111adfe78fb41bc02c12
maxime38955 <maxime38955@icloud.com>
2026-04-03 15:35:27 +0200

    Exo3 Boucle For

--- pieces.js
+++ pieces.js
@@ -4,22 +4,38 @@ const pieces = await reponse.json();
 
 const article = pieces[0];
 
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-
-
+for (let i = 0; i < pieces.length; i++) {
+ 
+// Récupération de l'élément du DOM qui accueillera les fiches
 const sectionFiches = document.querySelector(".fiches");
+// Création d’une balise dédiée à une pièce automobile
+const pieceElement = document.createElement("article");
+// On crée l’élément img.
+const imageElement = document.createElement("img");
+const nomElement = document.createElement("h2");
+const prixElement = document.createElement("p");
+const categorieElement = document.createElement("p");
+const descriptionElement = document.createElement("p");
+const disponibiliteElement = document.createElement("p");
+// On accède à l’indice i de la liste pieces pour configurer la source de l’image.
+imageElement.src = pieces[i].image;
+nomElement.innerText = pieces[i].nom;
+prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
+categorieElement.innerText = pieces[i].categorie;
+descriptionElement.innerText = pieces[i].description;
+disponibiliteElement.innerText = `${pieces[i].disponibilite == true ? "En stock" : "En rupture de stock"}`;
 
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
+// On rattache la balise article à la section Fiches
+sectionFiches.appendChild(pieceElement);
+
+pieceElement.appendChild(imageElement);
+pieceElement.appendChild(nomElement);
+pieceElement.appendChild(prixElement);
+pieceElement.appendChild(categorieElement);
+pieceElement.appendChild(descriptionElement);
+pieceElement.appendChild(disponibiliteElement);
+ 
+}
+
+
